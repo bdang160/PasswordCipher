83b081aa63c75c755d818bc9d81b974269459341
Bryan D <azangi700@gmail.com>
2015-06-02 11:13:36 -0400

    Added a system.out.println from local repository to Github

--- Cipher/src/projectElements/CipherMaker.java
+++ Cipher/src/projectElements/CipherMaker.java
@@ -3,5 +3,6 @@ package projectElements;
 public class CipherMaker {
   public static void main(String[]args) {
     System.out.println("From Github to local repository");
+    System.out.println("From local repository to Github");
   }
 }
